e0aecc3a7893a25a64f6b5833bb96ac60adf9419
dharms1004 <dharms1004@gmail.com>
2024-04-14 11:34:29 +0530

    typescript basics

---
+++ type-script/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let age = 32;
+let fName = "Dharm";
+let names = ["Dipika", "Surinder", "Sakshi"];
+let ages = [28, 30, 31];
+names.push("Ranjit");
+ages.push(35);
+console.log(age);
---
+++ type-script/src/index.ts
@@ -0,0 +1,14 @@
+let age = 32;
+let fName = "Dharm";
+
+
+let names: string[] = ["Dipika", "Surinder", "Sakshi"];
+
+let ages: number[] = [28, 30, 31];
+
+names.push("Ranjit");
+
+ages.push(35);
+
+  
+console.log(age);
